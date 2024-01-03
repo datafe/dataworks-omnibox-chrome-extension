13c4754b314ae2c87922cf4b73d826e0fc6749db
samuelhsin <caramels@gmail.com>
2024-01-04 02:19:24 +0800

    update pages

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -4,7 +4,8 @@ import 'webextension-polyfill';
 reloadOnUpdate('pages/background');
 
 const selfExtensionId = chrome?.runtime?.id;
-const links = [
+
+const bizLinks = [
   {
     keywords: [chrome.i18n.getMessage('inner'), 'inner', '弹内'],
     display: chrome.i18n.getMessage('inner'),
@@ -15,27 +16,6 @@ const links = [
       [chrome.i18n.getMessage('dataAnalytics')]: 'https://sqlx.alibaba-inc.com',
       [chrome.i18n.getMessage('dgc')]: 'https://dgc.dw.alibaba-inc.com',
       [chrome.i18n.getMessage('di')]: 'https://di.dw.alibaba-inc.com',
-    },
-  },
-  {
-    keywords: [chrome.i18n.getMessage('innerPre'), 'innerpre', '弹内预发'],
-    display: chrome.i18n.getMessage('innerPre'),
-    urlMap: {
-      [chrome.i18n.getMessage('dataStudio')]: 'https://pre-datastudio.dw.alibaba-inc.com',
-      [chrome.i18n.getMessage('dataMap')]: 'https://pre-dmc.dw.alibaba-inc.com',
-      [chrome.i18n.getMessage('dataService')]: 'https://pre-dataservice.dw.alibaba-inc.com',
-      [chrome.i18n.getMessage('dataAnalytics')]: 'https://pre-sqlx.alibaba-inc.com',
-      [chrome.i18n.getMessage('dgc')]: 'https://pre-dgc.alibaba-inc.com',
-      [chrome.i18n.getMessage('di')]: 'https://pre-di.alibaba-inc.com',
-    },
-  },
-  {
-    keywords: [chrome.i18n.getMessage('innerPreLocal'), 'innerprelocal', 'local', '弹内预发本地', '本地'],
-    display: chrome.i18n.getMessage('innerPreLocal'),
-    urlMap: {
-      [chrome.i18n.getMessage('dataService')]: 'https://local.pre-dataservice.alibaba-inc.com:8030/',
-      [chrome.i18n.getMessage('dgc')]: 'https://local.pre-dgc.dw.alibaba-inc.com:8002/',
-      [chrome.i18n.getMessage('tag')]: 'https://local.pre-dgc.dw.alibaba-inc.com:8000/tag',
     },
   },
   {
@@ -51,7 +31,7 @@ const links = [
     },
   },
   {
-    keywords: [chrome.i18n.getMessage('shanghaiPre'), 'shanghaipre', 'shangpre', '上海预发'],
+    keywords: [chrome.i18n.getMessage('shanghaiPre'), 'shanghai', 'pre', 'shang', '上海', '预发'],
     display: chrome.i18n.getMessage('shanghaiPre'),
     urlMap: {
       [chrome.i18n.getMessage('dataStudio')]: 'https://pre-ide2-cn-shanghai.data.aliyun.com',
@@ -60,24 +40,6 @@ const links = [
       [chrome.i18n.getMessage('dataAnalytics')]: 'https://pre-da-cn-shanghai.data.aliyun.com',
       [chrome.i18n.getMessage('dgc')]: 'https://pre-dgc-cn-shanghai.data.aliyun.com',
       [chrome.i18n.getMessage('dgc')]: 'https://pre-di-cn-shanghai.data.aliyun.com',
-    },
-  },
-  {
-    keywords: [chrome.i18n.getMessage('shanghaiPreLocal'), 'shanghaiprelocal', 'shangprelocal', '上海预发本地'],
-    display: chrome.i18n.getMessage('shanghaiPreLocal'),
-    urlMap: {
-      [chrome.i18n.getMessage('dataService')]: 'https://local.pre-ds-cn-shanghai.data.aliyun.com:8030/',
-      [chrome.i18n.getMessage('dgc')]: 'https://local.pre-dgc-cn-shanghai.data.aliyun.com:8002/',
-      [chrome.i18n.getMessage('tag')]: 'https://local.pre-dgc-cn-shanghai.data.aliyun.com:8000/tag',
-    },
-  },
-  {
-    keywords: [chrome.i18n.getMessage('shanghaiLocal'), 'shanghailocal', 'shanglocal', '上海本地'],
-    display: chrome.i18n.getMessage('shanghaiLocal'),
-    urlMap: {
-      [chrome.i18n.getMessage('dataService')]: 'https://local.prod-ds-cn-shanghai.data.aliyun.com:8030/',
-      [chrome.i18n.getMessage('dgc')]: 'https://local.prod-dgc-cn-shanghai.data.aliyun.com:8002/',
-      [chrome.i18n.getMessage('tag')]: 'https://local.prod-dgc-cn-shanghai.data.aliyun.com:8000/tag',
     },
   },
   {
@@ -90,20 +52,6 @@ const links = [
       [chrome.i18n.getMessage('dataAnalytics')]: 'https://da-cn-hangzhou.data.aliyun.com',
       [chrome.i18n.getMessage('dgc')]: 'https://dgc-cn-hangzhou.data.aliyun.com',
       [chrome.i18n.getMessage('di')]: 'https://di-cn-hangzhou.data.aliyun.com',
-    },
-  },
-  {
-    keywords: [chrome.i18n.getMessage('hangzhouPre'), 'hangzhoupre', 'hangpre', '杭州预发'],
-    display: chrome.i18n.getMessage('hangzhouPre'),
-    urlMap: {
-      [chrome.i18n.getMessage('dataService')]: 'https://pre-ds-vpc-cn-hangzhou.data.aliyun.com',
-    },
-  },
-  {
-    keywords: [chrome.i18n.getMessage('hangzhouPreLocal'), 'hangzhouprelocal', 'hangprelocal', '杭州预发本地'],
-    display: chrome.i18n.getMessage('hangzhouPreLocal'),
-    urlMap: {
-      [chrome.i18n.getMessage('dataService')]: 'https://local.pre-ds-cn-hangzhou.data.aliyun.com:8030/',
     },
   },
   {
@@ -128,20 +76,6 @@ const links = [
       [chrome.i18n.getMessage('dataAnalytics')]: 'https://da-cn-beijing.data.aliyun.com',
       [chrome.i18n.getMessage('dgc')]: 'https://dgc-cn-beijing.data.aliyun.com',
       [chrome.i18n.getMessage('di')]: 'https://di-cn-beijing.data.aliyun.com',
-    },
-  },
-  {
-    keywords: [chrome.i18n.getMessage('beijingPre'), 'beijingpre', 'beipre', '北京预发'],
-    display: chrome.i18n.getMessage('beijingPre'),
-    urlMap: {
-      [chrome.i18n.getMessage('dataService')]: 'https://pre-ds-vpc-cn-beijing.data.aliyun.com',
-    },
-  },
-  {
-    keywords: [chrome.i18n.getMessage('beijingPreLocal'), 'beijingprelocal', 'beiprelocal', '北京预发本地'],
-    display: chrome.i18n.getMessage('beijingPreLocal'),
-    urlMap: {
-      [chrome.i18n.getMessage('dataService')]: 'https://local.pre-ds-cn-beijing.data.aliyun.com:8030/',
     },
   },
   {
@@ -341,19 +275,174 @@ const links = [
   },
 ]
 
+const devBizLinks = [
+  {
+    keywords: [chrome.i18n.getMessage('innerPre'), 'inner', 'pre', '弹内', '预发'],
+    display: chrome.i18n.getMessage('innerPre'),
+    urlMap: {
+      [chrome.i18n.getMessage('dataStudio')]: 'https://pre-datastudio.dw.alibaba-inc.com',
+      [chrome.i18n.getMessage('dataMap')]: 'https://pre-dmc.dw.alibaba-inc.com',
+      [chrome.i18n.getMessage('dataService')]: 'https://pre-dataservice.dw.alibaba-inc.com',
+      [chrome.i18n.getMessage('dataAnalytics')]: 'https://pre-sqlx.alibaba-inc.com',
+      [chrome.i18n.getMessage('dgc')]: 'https://pre-dgc.alibaba-inc.com',
+      [chrome.i18n.getMessage('di')]: 'https://pre-di.alibaba-inc.com',
+    },
+  },
+  {
+    keywords: [chrome.i18n.getMessage('innerPreLocal'), 'inner', 'pre', 'local', '弹内', '预发', '本地'],
+    display: chrome.i18n.getMessage('innerPreLocal'),
+    urlMap: {
+      [chrome.i18n.getMessage('dataService')]: 'https://local.pre-dataservice.alibaba-inc.com:8030',
+      [chrome.i18n.getMessage('dgc')]: 'https://local.pre-dgc.dw.alibaba-inc.com:8002',
+      [chrome.i18n.getMessage('tag')]: 'https://local.pre-dgc.dw.alibaba-inc.com:8000/tag',
+    },
+  },
+  {
+    keywords: [chrome.i18n.getMessage('shanghaiPreLocal'), 'shanghai', 'pre', 'local', 'shang', '上海', '预发', '本地'],
+    display: chrome.i18n.getMessage('shanghaiPreLocal'),
+    urlMap: {
+      [chrome.i18n.getMessage('dataService')]: 'https://local.pre-ds-cn-shanghai.data.aliyun.com:8030',
+      [chrome.i18n.getMessage('dgc')]: 'https://local.pre-dgc-cn-shanghai.data.aliyun.com:8002',
+      [chrome.i18n.getMessage('tag')]: 'https://local.pre-dgc-cn-shanghai.data.aliyun.com:8000/tag',
+    },
+  },
+  {
+    keywords: [chrome.i18n.getMessage('shanghaiLocal'), 'shanghai', 'local', 'shang', 'local', '上海', '本地'],
+    display: chrome.i18n.getMessage('shanghaiLocal'),
+    urlMap: {
+      [chrome.i18n.getMessage('dataService')]: 'https://local.prod-ds-cn-shanghai.data.aliyun.com:8030',
+      [chrome.i18n.getMessage('dgc')]: 'https://local.prod-dgc-cn-shanghai.data.aliyun.com:8002',
+      [chrome.i18n.getMessage('tag')]: 'https://local.prod-dgc-cn-shanghai.data.aliyun.com:8000/tag',
+    },
+  },
+  {
+    keywords: [chrome.i18n.getMessage('hangzhouPre'), 'hangzhou', 'pre', 'hang', '杭州', '预发'],
+    display: chrome.i18n.getMessage('hangzhouPre'),
+    urlMap: {
+      [chrome.i18n.getMessage('dataService')]: 'https://pre-ds-vpc-cn-hangzhou.data.aliyun.com',
+    },
+  },
+  {
+    keywords: [chrome.i18n.getMessage('hangzhouPreLocal'), 'hangzhou', 'pre', 'local', 'hang', '杭州', '预发', '本地'],
+    display: chrome.i18n.getMessage('hangzhouPreLocal'),
+    urlMap: {
+      [chrome.i18n.getMessage('dataService')]: 'https://local.pre-ds-cn-hangzhou.data.aliyun.com:8030',
+    },
+  },
+  {
+    keywords: [chrome.i18n.getMessage('beijingPre'), 'beijing', 'bei', 'pre', '北京', '预发'],
+    display: chrome.i18n.getMessage('beijingPre'),
+    urlMap: {
+      [chrome.i18n.getMessage('dataService')]: 'https://pre-ds-vpc-cn-beijing.data.aliyun.com',
+    },
+  },
+  {
+    keywords: [chrome.i18n.getMessage('beijingPreLocal'), 'beijing', 'pre', 'local', 'bei', '北京', '预发', '本地'],
+    display: chrome.i18n.getMessage('beijingPreLocal'),
+    urlMap: {
+      [chrome.i18n.getMessage('dataService')]: 'https://local.pre-ds-cn-beijing.data.aliyun.com:8030',
+    },
+  },
+  // 其他
+  {
+    keywords: [chrome.i18n.getMessage('uiCodeInventory'), 'code', 'fe'],
+    display: chrome.i18n.getMessage('uiCodeInventory'),
+    urlMap: {
+      [chrome.i18n.getMessage('dataStudio')]: 'https://code.alibaba-inc.com/alishu/datastudio',
+      [chrome.i18n.getMessage('dataService')]: 'https://code.alibaba-inc.com/alishu/only-dataservice',
+      [chrome.i18n.getMessage('dataMap')]: 'https://code.alibaba-inc.com/alishu/only-datamap',
+      [chrome.i18n.getMessage('dataAnalytics')]: 'https://code.alibaba-inc.com/alishu/only-excel',
+      [chrome.i18n.getMessage('di')]: 'https://code.alibaba-inc.com/alishu/xstudio-di',
+      [chrome.i18n.getMessage('dgc')]: 'https://code.alibaba-inc.com/alishu/data-governance-center',
+    },
+  },
+  {
+    keywords: [chrome.i18n.getMessage('ued'), 'ued', 'design'],
+    display: chrome.i18n.getMessage('ued'),
+    urlMap: {
+      [chrome.i18n.getMessage('dgc')]: 'https://mgdone.alibaba-inc.com/file/112913719891129',
+    },
+  },
+  {
+    keywords: [chrome.i18n.getMessage('prd'), 'prd'],
+    display: chrome.i18n.getMessage('prd'),
+    urlMap: {
+      [chrome.i18n.getMessage('dgc')]: 'https://aliyuque.antfin.com/dataworks/dybyo0',
+    },
+  },
+];
+
+const devLinks = [
+  {
+    keywords: ['dwuri', 'command'],
+    display: chrome.i18n.getMessage('dwUri'),
+    urlMap: {
+      common: 'https://dcd.alibaba-inc.com/?env=prod#/help-doc/1-6#%E6%B3%A8%E5%86%8C%E4%BF%A1%E6%81%AF',
+    },
+  },
+  {
+    keywords: ['color'],
+    display: chrome.i18n.getMessage('uiPalette'),
+    urlMap: {
+      common: 'https://dcd.alibaba-inc.com/?env=prod#/palette',
+    },
+  },
+  {
+    keywords: ['icon'],
+    display: chrome.i18n.getMessage('uiIcon'),
+    urlMap: {
+      common: 'https://dcd.alibaba-inc.com/?env=prod#/icon-search',
+    },
+  },
+  {
+    keywords: ['aone'],
+    display: chrome.i18n.getMessage('aone'),
+    urlMap: {
+      common: 'https://aone.alibaba-inc.com/v2/req/__var1__',
+    },
+  },
+];
+
 const products = [
   { keywords: [chrome.i18n.getMessage('dataStudio'), 'ide', 'studio', '开发'], content: chrome.i18n.getMessage('dataStudio'), description: chrome.i18n.getMessage('dataStudio') },
   { keywords: [chrome.i18n.getMessage('dataMap'), 'dmc', 'map', '地图'], content: chrome.i18n.getMessage('dataMap'), description: chrome.i18n.getMessage('dataMap') },
-  { keywords: [chrome.i18n.getMessage('dataService'), 'ds', 'service', '服务'], content: chrome.i18n.getMessage('dataService'), description: chrome.i18n.getMessage('dataService') },
+  { keywords: [chrome.i18n.getMessage('dataService'), 'dataservice', 'ds', '服务'], content: chrome.i18n.getMessage('dataService'), description: chrome.i18n.getMessage('dataService') },
   { keywords: [chrome.i18n.getMessage('dataAnalytics'), 'da', 'sqlx', 'analyze', 'analytics', '分析'], content: chrome.i18n.getMessage('dataAnalytics'), description: chrome.i18n.getMessage('dataAnalytics') },
   { keywords: [chrome.i18n.getMessage('dgc'), 'dgc', '治理'], content: chrome.i18n.getMessage('dgc'), description: chrome.i18n.getMessage('dgc') },
   { keywords: [chrome.i18n.getMessage('tag'), 'tag', '标签'], content: chrome.i18n.getMessage('tag'), description: chrome.i18n.getMessage('tag') },
-  { keywords: [chrome.i18n.getMessage('di'), 'integrate', '集成'], content: chrome.i18n.getMessage('di'), description: chrome.i18n.getMessage('di') },
+  { keywords: [chrome.i18n.getMessage('di'), 'di', 'integrate', '集成'], content: chrome.i18n.getMessage('di'), description: chrome.i18n.getMessage('di') },
 ];
 
-chrome.omnibox.setDefaultSuggestion({
-  description: chrome.i18n.getMessage('console'),
-});
+// chrome.omnibox.setDefaultSuggestion({
+//   description: chrome.i18n.getMessage('console'),
+// });
+
+const intersection = (array1 = [], array2 = []) => {
+  return array1?.filter?.(value => array2?.includes?.(value)) || [];
+}
+
+const notIntersection = (array1 = [], array2 = []) => {
+  const inter = intersection(array1, array2);
+  let result = [];
+
+  if (inter?.length > 0) {
+    array1?.forEach?.(value => {
+      if (!inter?.includes?.(value)) {
+        result?.push?.(value);
+      }
+    });
+    array2?.forEach?.(value => {
+      if (!inter?.includes?.(value)) {
+        result?.push?.(value);
+      }
+    });
+  } else {
+    result = [...array1, ...array2];
+  }
+  return result;
+}
+
+let tempText;
 
 // default suggestion
 chrome.omnibox.onInputStarted.addListener(function () {
@@ -361,23 +450,9 @@ chrome.omnibox.onInputStarted.addListener(function () {
 
 chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
 
-  // parse region
-  const regionResult = links.filter(item => {
-    return item?.keywords?.findIndex?.(keyword => {
-      if (text?.includes?.(keyword) || text === keyword) return true;
-      else if (keyword?.startsWith?.(text)) return true;
-    }) !== -1;
-  }) || [];
+  const textArr = text?.split?.(' ') || [];
 
-  // parse product
-  const productResult = products.filter(item => {
-    return item?.keywords?.findIndex?.(keyword => {
-      if (text?.includes?.(keyword) || text === keyword) return true;
-      else if (keyword?.startsWith?.(text)) return true;
-    }) !== -1;
-  }) || [];
-
-  const set = new Set<{ content: string; description: string; deletable?: boolean }>();
+  const set = new Set<{ content: string; description: string; deletable?: boolean; searchedCounts?: number }>();
 
   const calMatch = (str: string, keyword: string) => {
     if (!keyword) return str;
@@ -385,51 +460,110 @@ chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
     return str?.replaceAll ? str?.replaceAll?.(_keyword, `<match><dim>${_keyword}</dim></match>`) : str;
   }
 
-  regionResult.forEach(region => {
+  const allBizLinks = bizLinks.concat(devBizLinks);
 
-    let restText = text?.trim?.();
-    region?.keywords?.forEach?.(keyword => {
-      restText = restText?.replace?.(keyword, '')?.trim?.();
-    });
-
+  allBizLinks.forEach(region => {
     products.forEach(product => {
       if (!region?.urlMap?.[product.content]) return;
-      const description = `${region.display} ${product.description} <url>${region.urlMap?.[product.content]}</url>`;
-      if (restText) {
-        if (product?.keywords?.includes?.(restText)) {
-          set.add({ content: `${region.display} ${product.content}`, description: calMatch(description, text) });
-        }
-      } else {
-        set.add({ content: `${region.display} ${product.content}`, description: calMatch(description, text) });
+      const searched = intersection((region?.keywords || [])?.concat?.(product?.keywords || []), textArr);
+      if (searched?.length > 0) {
+        const description = `${region.display} ${product.description} <url>${region.urlMap?.[product.content]}</url>`;
+        set.add({ content: `${region.display} ${product.content}`, description: calMatch(description, text), searchedCounts: searched?.length || 0 });
       }
     });
   });
 
-  productResult.forEach(product => {
-    links.forEach(region => {
+  devLinks.forEach(region => {
+
+    if (region?.urlMap?.common) {
+      const searched = intersection((region?.keywords || []), textArr);
+      if (searched?.length > 0) {
+        const content = region.display;
+        const description = `${region.display} <url>${region.urlMap?.common}</url>`;
+        set.add({ content, description: calMatch(description, text), searchedCounts: searched?.length || 0 });
+      }
+    }
+
+    products.forEach(product => {
       if (!region?.urlMap?.[product.content]) return;
-      const description = `${region.display} ${product.description} <url>${region.urlMap?.[product.content]}</url>`;
-      set.add({ content: `${region.display} ${product.content}`, description: calMatch(description, text) });
+      const searched = intersection((region?.keywords || [])?.concat?.(product?.keywords || []), textArr);
+      if (searched?.length > 0) {
+        const content = `${region.display} ${product.content}`;
+        const description = `${region.display} ${product.description} <url>${region.urlMap?.[product.content]}</url>`;
+        set.add({ content, description: calMatch(description, text), searchedCounts: searched?.length || 0 });
+      }
     });
   });
 
   const result: chrome.omnibox.SuggestResult[] = Array.from(set) || [];
+
+  result.sort((a, b) => (b as any)?.searchedCounts - (a as any)?.searchedCounts);
+  result?.forEach?.((item: any) => delete item?.searchedCounts); // remove searchedCounts
+
   suggest(result);
+
+  tempText = text;
+
 });
 
 chrome.omnibox.onInputEntered.addListener(function (text, disposition) {
 
+  const textArr = text?.split?.(' ') || [];
+  let restVars = [];
+
   let link;
-  links.findIndex(region => {
+
+  const allBizLinks = bizLinks.concat(devBizLinks);
+
+  allBizLinks.findIndex(region => {
     return products?.findIndex?.(product => {
-      if (text === `${region.display} ${product.content}`) {
+      const content = `${region.display} ${product.content}`;
+      if (text === content) {
         link = region.urlMap[product.content];
         return true;
       }
     }) !== -1;
   });
 
-  if (link) chrome?.tabs?.create?.({ url: link });
+  if (!link) {
+    devLinks.findIndex(region => {
+      let existed = products?.findIndex?.(product => {
+        const content = `${region.display} ${product.content}`;
+        if (text === content) {
+          link = region.urlMap[product.content];
+          return true;
+        }
+      }) !== -1;
+
+      if (existed) return true;
+
+      if (region?.urlMap?.common) {
+
+        if (text === region?.display) {
+          link = region?.urlMap?.common;
+          restVars = notIntersection(region?.keywords, tempText?.split?.(' ') || []);
+          return true;
+        } else if (intersection(region?.keywords, textArr)?.length > 0) {
+          link = region?.urlMap?.common;
+          restVars = notIntersection(region?.keywords, textArr);
+          return true;
+        }
+      }
+    });
+  }
+
+  if (link) {
+
+    // check path var, starting from 1
+    for (let i = 1; i < 10; i++) {
+      const flag = `__var${i}__`;
+      if (link?.includes?.(flag)) {
+        if (restVars?.[i - 1]) link = link?.replaceAll?.(flag, restVars?.[i - 1] || '');
+      } else break;
+    }
+
+    chrome?.tabs?.create?.({ url: link });
+  }
   else chrome?.tabs?.create?.({ url: 'https://dataworks.console.aliyun.com/' }); // default 至管控台
 
 });
