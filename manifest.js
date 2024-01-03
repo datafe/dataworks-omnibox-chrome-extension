8b595697c35e9699cee65faa2c689760d6d674f2
samuelhsin <caramels@gmail.com>
2024-01-04 02:16:45 +0800

    update

--- manifest.js
+++ manifest.js
@@ -11,24 +11,21 @@ const manifest = {
   description: '__MSG_appDescription__',
   omnibox: { 'keyword': 'dw' },
   permissions: [],
-  // options_page: 'src/pages/options/index.html',
   default_locale: "zh_CN",
   background: {
     service_worker: 'src/pages/background/index.js',
     type: 'module',
   },
   icons: {
+    225: 'logo-225.png',
     128: 'logo-128.png',
     64: 'logo-64.png',
     32: 'logo-32.png',
     16: 'logo-16.png'
   },
-  web_accessible_resources: [
-    {
-      resources: ['assets/js/*.js', 'assets/css/*.css', 'icon-128.png', 'icon-32.png'],
-      matches: ['*://*/*'],
-    },
-  ],
+  content_security_policy: {
+    extension_pages: "script-src 'self'; object-src 'self';",
+  },
 };
 
 export default manifest;
